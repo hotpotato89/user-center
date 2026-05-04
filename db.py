baae6db1ba09fdbcbcb3d0d920a3f14c517c72a1
hotpotato <ramilreading@gmail.com>
2026-05-04 20:37:23 +0300

    сделал основу

---
+++ db.py
@@ -0,0 +1,58 @@
+import asyncpg
+import os
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+import schemas
+
+load_dotenv()
+
+dsn = os.getenv('DATABASE_URL')
+if not dsn:
+    raise ValueError('Нет DATABASE_URL в файле .env')
+admin_password = os.getenv('ADMIN_PASSWORD')
+if not admin_password:
+    raise ValueError('Нет ADMIN_PASSWORD в файле .env')
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.pool = await asyncpg.create_pool(
+        dsn=dsn,
+        min_size=5,
+        max_size=10,
+    )
+    print('Соединение с базой данных установлено')
+    async with app.state.pool.acquire() as session:
+        await session.execute('create table if not exists users (id serial primary key, name varchar(60), age integer, email varchar(45) unique, reg_time timestamp default now())')
+    yield
+    print('Соединение с базой данных разорвано')
+    await app.state.pool.close()
+
+async def get_users_db(pool):
+    async with pool.acquire() as session:
+        data = await session.fetch('select * from users order by reg_time desc')
+    if not data:
+        return schemas.ReturnForm(success=False, message='База данных пуста', error_code='empty')
+    return schemas.ReturnForm(success=True, message=f'Найдено {len(data)} пользователей.', data=[dict(row) for row in data])
+
+async def add_user_db(pool, user_data: schemas.UserDataForm):
+    async with pool.acquire() as session:
+        try:
+            user_id = await session.fetchval('insert into users (name, age, email) values ($1, $2, $3) returning id', user_data.name, user_data.age, user_data.email)
+            return schemas.ReturnForm(success=True, message=f'Успешно добавлен пользователь по айди {user_id}')
+        except asyncpg.UniqueViolationError:
+            return schemas.ReturnForm(success=False, message=f'Пользователь с email \'{user_data.email}\' уже существует', error_code='conflict')
+        except Exception as e:
+            print(f'Ошибка базы данных {e}')
+            return schemas.ReturnForm(success=False, message='Ошибка на стороне сервера')
+        
+async def clear_all_db(pool, password: schemas.PasswordForm):
+    if password.password != admin_password:
+        return schemas.ReturnForm(success=False, message='Неверный админ-пароль', error_code='unauthorized')
+    async with pool.acquire() as session:
+        deleted = await session.fetchval('select count(*) from users')
+        if deleted == 0:
+            return schemas.ReturnForm(success=False, message='База данных итак пуста', error_code='empty')
+        await session.execute('truncate users restart identity')
+    return schemas.ReturnForm(success=True, message=f'Удалено {deleted} записей.')
---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Request, Depends, HTTPException
+from time import perf_counter
+
+import db
+import schemas
+
+app = FastAPI(lifespan=db.lifespan, title='Моя API')
+main_tag: list = ['Моя API']
+
+async def get_pool():
+    return app.state.pool
+
+@app.middleware('http')
+async def middleware_(request: Request, next_call):
+    start_time = perf_counter()
+    result = await next_call(request)
+    duration = perf_counter() - start_time
+    if duration > 0.1:
+        print(f'Медленный запрос, заняло {duration:.4f}')
+    return result
+
+@app.get('/', tags=main_tag)
+async def main_page():
+    return schemas.ReturnForm(success=True, message='Успешно')
+
+@app.get('/users', tags=main_tag)
+async def get_users(pool = Depends(get_pool)):
+    result = await db.get_users_db(pool)
+    if result.success != True:
+        if result.error_code == 'empty':
+            raise HTTPException(status_code=404, detail=result.message)
+        raise HTTPException(status_code=500, detail=result.message)
+    return result
+
+@app.post('/add_user', tags=main_tag)
+async def add_user(user_data: schemas.UserDataForm, pool=Depends(get_pool)):
+    result = await db.add_user_db(pool, user_data)
+    if result.success != True:
+        if result.error_code == 'conflict':
+            raise HTTPException(status_code=409, detail=result.message)
+        raise HTTPException(status_code=500, detail=result.message)
+    return result
+
+@app.delete('/clear', tags=main_tag)
+async def clear_all(password: schemas.PasswordForm, pool = Depends(get_pool)):
+    result = await db.clear_all_db(pool, password)
+    if not result.success:
+        if result.error_code == 'unauthorized':
+            raise HTTPException(status_code=401, detail=result.message)
+        if result.error_code == 'empty':
+            raise HTTPException(status_code=400, detail=result.message)
+        raise HTTPException(status_code=500, detail=result.message)
+    return result
